c81590051e58468e8d15a2c73845f1899b7e0dd0
github username <github email address>
2018-07-07 13:09:01 -0500

    added join columns to pledges

--- db/migrate/20180707173946_create_pledges.rb
+++ db/migrate/20180707173946_create_pledges.rb
@@ -2,6 +2,8 @@ class CreatePledges < ActiveRecord::Migration[5.2]
   def change
     create_table :pledges do |t|
       t.string :amount
+      t.integer :user_id
+      t.integer :funding_project_id
       t.string :created_at
       t.string :updated_at
       t.timestamps
