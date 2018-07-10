bd203b0abef1f88ab84bbf57b56c2edbf13c6b61
github username <github email address>
2018-07-10 13:05:17 -0500

    finishing first draft userscontroller, building users#new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,15 +14,20 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.update(user_params)
+
   end
 
   def destroy
+    @user.delete
   end
 
   private
