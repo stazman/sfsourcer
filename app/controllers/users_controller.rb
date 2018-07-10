1501f2ce4ff21e451aa9b767c302d536897aa0ae
github username <github email address>
2018-07-10 12:57:35 -0500

    building out userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to users_path(@user)
   end
 
   def show
@@ -18,5 +23,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :comment_attributes, :pledge_attributes)
   end
 end
