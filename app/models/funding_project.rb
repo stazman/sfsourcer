abfed697cef5e3a8f5359e186b59bcb6b5bddedb
github username <github email address>
2018-07-11 08:48:51 -0500

    building out validations in models

--- app/models/funding_project.rb
+++ app/models/funding_project.rb
@@ -2,10 +2,10 @@ class FundingProject < ApplicationRecord
     has_many :pledges
     has_many :users, through: :pledges
 
-    validates presence of title
-    validates uniqueness of title
-    validates presence of description
-    validates presence of funding_amount
+    validates_presence_of :title
+    validates_uniqueness_of :title
+    validates_presence_of :description
+    validates_presence_of :funding_amount
 
     # def comment_attributes=(comment_attribute)
     # end
