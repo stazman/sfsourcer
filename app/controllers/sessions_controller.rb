1f91cf4594476efaa0ed06e7d4770509e562d0e7
github username <github email address>
2018-07-11 12:58:45 -0500

    debugging login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,17 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by(email: params[:email])
-        if user && user.authenticate(params[:email][:password])
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
-        else
-            redirect_to '/login'
-        end
+        user = User.find_by(email: params[:user][:email])
+        user = user.try(:authenticate, params[:user][:password])
+
+        return redirect_to(controller: 'sessions', action: 'new') unless user
+
+        #why parentheses here and not with redirect_to below?
+
+        session[:user_id] = user.id
+
+        @user = user
+        redirect_to user_path(@user)
     end
 
     def destroy
