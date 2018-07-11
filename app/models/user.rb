4ec770bda83d7c34047b229368c7e071d733bca6
github username <github email address>
2018-07-11 09:48:09 -0500

    adding make_name_titlecase functionality to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
     validates_presence_of :password
     validates_presence_of :email
     validates_uniqueness_of :email
+
+    def make_name_titlecase
+        self.name = self.name.titlecase
+    end
 end
