7f548c7295e03fb994f8c8e38cdb902b9e584339
github username <github email address>
2018-07-11 09:15:00 -0500

    making sure validations for user work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.valid?
       @user.save
-      redirect_to users_path(@user)
+      redirect_to user_path(@user)
     else
       render :new
     end
