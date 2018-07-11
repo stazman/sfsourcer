a506bd334306715ed2755a7fcba114b02f6b7605
github username <github email address>
2018-07-11 15:45:59 -0500

    stubbing out fundingcontroller

--- app/controllers/funding_projects_controller.rb
+++ app/controllers/funding_projects_controller.rb
@@ -1,25 +1,30 @@
 class FundingProjectsController < ApplicationController
   before_save :make_titlecase
   
+
+
+
   def index
-    
+    @funding_projects = FundingProject.all
   end
 
   def new
+    #make new FP instance possible
   end
 
   def create
+    #create and save new FP instance
   end
 
   def show
+    #show newly created FP instance
   end
 
   def edit
+    #make editong an already made FP instance possible
   end
 
   def update
-  end
-
-  def destroy
+    #update an already made FP instance, including saving it
   end
 end
