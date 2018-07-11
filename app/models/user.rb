8486a50f699f55437bff7bf4411a6535df53259d
github username <github email address>
2018-07-11 12:04:02 -0500

    debugging login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,16 @@
 class User < ApplicationRecord
     has_many :pledges
     has_many :funding_projects, through: :pledges
+    has_secure_password
 
     validates_presence_of :name
     validates_presence_of :password
     validates_presence_of :email
     validates_uniqueness_of :email
+
+    before_save :make_name_titlecase
+
+private
 
     def make_name_titlecase
         self.name = self.name.titlecase
