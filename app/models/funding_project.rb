982f9bbe0f091f0ffeeb111713ce2e8a3ed283a4
github username <github email address>
2018-07-12 13:28:02 -0500

    associated newly created pledges with a funding_project

--- app/models/funding_project.rb
+++ app/models/funding_project.rb
@@ -6,11 +6,18 @@ class FundingProject < ApplicationRecord
     validates_uniqueness_of :title
     validates_presence_of :description
     validates_presence_of :funding_goal
-
+    
     def make_title_case
         self.title = self.title.titlecase
     end
 
+    def funding_project_pledge=(funding_project_pledge)
+        self.pledge = Pledge.create_by(amount: funding_project_pledge)
+    end
+    
+    def funding_project_pledge
+        self.pledge ? self.pledge.amount : nil
+    end
     # def comment_attributes=(comment_attribute)
     # end
 end
