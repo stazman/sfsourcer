20dad6db89b0f2dd4bd3cf68d8c479215ca8630a
github username <github email address>
2018-07-15 17:28:26 -0500

    excepted pledges#index from routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
     has_many :pledges
     has_many :funding_projects, through: :pledges
-    # has_secure_password
+    has_secure_password
 
-    # validates_presence_of :name
-    # validates_presence_of :password
-    # validates_presence_of :email
-    # validates_uniqueness_of :email
+    validates_presence_of :name
+    validates_presence_of :password
+    validates_presence_of :email
+    validates_uniqueness_of :email
 
     # before_save :make_name_titlecase
 
