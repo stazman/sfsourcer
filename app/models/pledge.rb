010c8080ea9d07a60a0c3d68f1091ca05db8216d
github username <github email address>
2018-07-15 19:22:31 -0500

    making form for pledges and a custom writer to associate pledges with funding_projects

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -4,5 +4,9 @@ class Pledge < ApplicationRecord
 
     validates_presence_of :amount
     validates :amount, numericality: true
+
+    def project_pledges=(project_pledge)
+        self.funding_project = FundingProject.find_by(title: funding.title)
+    end
     #add validation for datatype integer
 end
