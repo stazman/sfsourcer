de914f82fd330dd39fefd093bea81d46e69e5eab
github username <github email address>
2018-07-16 18:23:28 -0500

    working on new solution to pledges/projects association issue

--- app/models/funding_project.rb
+++ app/models/funding_project.rb
@@ -1,23 +1,12 @@
 class FundingProject < ApplicationRecord
     has_many :pledges
     has_many :users, through: :pledges
-    accepts_nested_attributes_for :pledges
-    
-    
-    def funding_project_attributes=(funding_project_attributes)
-        funding_project_attributes.values.each do |funding_project_attribute|
-          funding_project = FundingProject.find_or_create_by(funding_project_attributes)
-          self.funding << category
-    end
-
 
     validates_presence_of :title
     validates_uniqueness_of :title
     validates_presence_of :description
     validates_presence_of :funding_goal
     
-    
-
     def make_title_case
         self.title = self.title.titlecase
     end
--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,14 +1,11 @@
 class Pledge < ApplicationRecord
     belongs_to :user
     belongs_to :funding_project
-
-     
+    
+    def funding_project_title=(funding_project_title)
+        self.funding_project = FundingProject.find_by(title: funding_project_title)
+    end
 
     validates_presence_of :amount
     validates :amount, numericality: true
-
-    # def project_pledges=(project_pledge)
-    #     self.funding_project = FundingProject.find_by(title: funding.title)
-    # end
-    #add validation for datatype integer
 end
