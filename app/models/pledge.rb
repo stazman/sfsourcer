1bca82a25bf4b457ceeecdf0b88ad3b5cbe3fb3d
github username <github email address>
2018-07-17 16:48:55 -0500

    fixed login issue and pledge/project association issue

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -3,7 +3,7 @@ class Pledge < ApplicationRecord
     belongs_to :funding_project
     
     def funding_project_title=(funding_project_title)
-        self.funding_project = FundingProject.find_by(title: funding_project_title)
+        self.funding_project.title = FundingProject.find_by(title: funding_project_title)
     end
 
     validates_presence_of :amount
