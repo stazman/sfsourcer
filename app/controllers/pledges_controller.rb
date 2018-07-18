9c86e683e4861dae06e39427749a246f78bcf13c
github username <github email address>
2018-07-18 15:40:55 -0500

    login error messages for all classes showing on login page

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -43,7 +43,7 @@ private
 
   def require_login
     unless logged_in?
-      flash[:error] = "You must be logged in to access this section"
+      flash[:alert] = "You must be logged in to access this section"
       redirect_to login_path
     end
   end
