7d0d4a0765aa3cadb77ae78af485cef345ac77da
github username <github email address>
2018-07-18 15:22:13 -0500

    getting login error messages to show on login page

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -44,7 +44,7 @@ private
   def require_login
     unless logged_in?
       flash[:error] = "You must be logged in to access this section"
-      redirect_to new_login_url
+      redirect_to login_path
     end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
   def require_login
     unless logged_in?
       flash[:error] = "You must be logged in to access this section"
-      redirect_to new_login_url
+      redirect_to login_url
     end
   end
 end
