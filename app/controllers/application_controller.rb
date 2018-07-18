5e98f0a17a0d632c2046c017e986883e5256b44d
github username <github email address>
2018-07-18 15:22:13 -0500

    getting login error messages to show on login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base    
     def current_user
-        # return unless session[:user_id]
-        # @current_user ||= 
-        User.find(session[:user_id])
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
     end
 
     def logged_in?
