e742c7b01007053a59bd79c82dc877d7e4afcdce
github username <github email address>
2018-07-19 17:51:40 -0500

    making user address a nested resource, setting up nested attributes, added user_id to addresses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_19_214344) do
+ActiveRecord::Schema.define(version: 2018_07_19_224842) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "address1"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_07_19_214344) do
     t.integer "zip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
