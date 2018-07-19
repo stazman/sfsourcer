9d77fbf4ae3432bc92912fd1507533da1c1a472d
github username <github email address>
2018-07-19 17:12:16 -0500

    migrated addresses

--- db/migrate/20180719214344_create_addresses.rb
+++ db/migrate/20180719214344_create_addresses.rb
@@ -1,7 +1,11 @@
 class CreateAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :addresses do |t|
-
+      t.string :address1
+      t.string :address2
+      t.string :city
+      t.string :state
+      t.integer :zip
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_18_221422) do
+ActiveRecord::Schema.define(version: 2018_07_19_214344) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "address1"
+    t.string "address2"
+    t.string "city"
+    t.string "state"
+    t.integer "zip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string "title"
