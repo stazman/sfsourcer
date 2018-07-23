aa37544135d639f0e18fc994033bf75916cf5f21
github username <github email address>
2018-07-23 08:43:27 -0500

    getting new comments to associate with their funding_project

--- app/models/funding_project.rb
+++ app/models/funding_project.rb
@@ -3,6 +3,7 @@ class FundingProject < ApplicationRecord
     has_many :users, through: :pledges
     has_many :comments
     has_many :comments, through: :users
+    # accepts_nested_attributes_for :comments
 
     validates_presence_of :title
     validates_uniqueness_of :title
