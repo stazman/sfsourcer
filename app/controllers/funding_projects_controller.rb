ce5bd76be7795e0ce77e1b19a7f82bfe862dc4ee
github username <github email address>
2018-07-25 10:19:02 -0500

    debugging no id persisted for new funding_project

--- app/controllers/funding_projects_controller.rb
+++ app/controllers/funding_projects_controller.rb
@@ -21,7 +21,8 @@ class FundingProjectsController < ApplicationController
 
   def new
     @funding_project = FundingProject.new
-    @funding_project.comments.build
+    # @funding_project.comments.build
+    # @funding_project.user_id = current_user
     # @comment = @comment.find_by(:id)
     # @funding_project.id = @comment.fundng_project_id
     # @funding_project.comments.build
@@ -29,14 +30,16 @@ class FundingProjectsController < ApplicationController
 
   def create
     @funding_project = FundingProject.new(funding_project_params)
+    if @funding_project.valid?
+      # fp_pledges = @funding_project.pledges
+      @funding_project.save
+  
+      redirect_to funding_project_path(@funding_project)
+    else
+      render :new
+    end
 
-    @funding_project.save
 
-    @funding_project.current_user = current_user
-
-    fp_pledges = @funding_project.pledges
-    
-    redirect_to funding_project_path(@funding_project)
   end
 
   def show
@@ -59,7 +62,7 @@ class FundingProjectsController < ApplicationController
   private
 
   def funding_project_params
-    params.require(:funding_project).permit(:funding_project_pledge, :creator, :title, :description, :funding_goal, comments_attributes: [:title, :content] )
+    params.require(:funding_project).permit(:funding_project_pledge, :user_id, :creator, :title, :description, :funding_goal, comments_attributes: [:title, :content] )
   end
 
   def require_login
