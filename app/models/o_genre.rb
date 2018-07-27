d2d27e3f2517de0b3b2482a9a83116b39861668b
github username <github email address>
2018-07-27 18:00:54 -0500

    removed all files/code for fp_creator bec including in fp model; reorganizing assocations with user, l_story, o_genre

--- app/models/o_genre.rb
+++ app/models/o_genre.rb
@@ -1,5 +1,5 @@
 class OGenre < ApplicationRecord
-    belongs_to :l_story
-    # has_many :o_genres
-    # has_many :l_stories, through: :l_story_o_genres
+    has_many :l_story_genres
+    has_many :l_stories, through: :l_story_o_genres
+    has_many :users, through: :l_stories
 end
