f9656b854f17d351b22fe4cab8cc935e3f9fe768
github username <github email address>
2018-07-27 16:06:48 -0500

    making a class method for funding projects that are within 80 percent of funding goal

--- app/models/o_genre.rb
+++ app/models/o_genre.rb
@@ -1,5 +1,5 @@
 class OGenre < ApplicationRecord
-    # belongs_to :l_story
-    has_many :l_story_o_genres
-    has_many :l_stories, through: :l_story_o_genres
+    belongs_to :l_story
+    # has_many :o_genres
+    # has_many :l_stories, through: :l_story_o_genres
 end
