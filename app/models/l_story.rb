e691014c6b881e57f17982596b9d41b9a6d2d45e
github username <github email address>
2018-07-27 12:52:17 -0500

    getting new l_story to instantiate

--- app/models/l_story.rb
+++ app/models/l_story.rb
@@ -2,4 +2,8 @@ class LStory < ApplicationRecord
     has_many :l_story_o_genres
     has_many :o_genres, through: :l_story_o_genres
     accepts_nested_attributes_for :o_genres
+
+    validates_presence_of :title
+    validates_presence_of :content
+
 end
