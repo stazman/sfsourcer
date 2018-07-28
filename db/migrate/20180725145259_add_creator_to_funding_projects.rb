43497f1332e8ea5453053b93e5be19c3f625cece
github username <github email address>
2018-07-28 11:27:25 -0500

    removing fp_creator code from app

--- db/migrate/20180725145259_add_creator_to_funding_projects.rb
+++
@@ -1,5 +0,0 @@
-class AddCreatorToFundingProjects < ActiveRecord::Migration[5.2]
-  def change
-    add_column :funding_projects, :creator, :string
-  end
-end
--- db/migrate/20180725213153_add_fp_creator_id_to_funding_projects.rb
+++
@@ -1,5 +0,0 @@
-class AddFpCreatorIdToFundingProjects < ActiveRecord::Migration[5.2]
-  def change
-    add_column :funding_projects, :fp_creator_id, :integer
-  end
-end
