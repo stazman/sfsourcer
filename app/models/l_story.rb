24a2a1d39d8ad29c98c8d895f7adacf882ab4c86
github username <github email address>
2018-07-28 13:45:15 -0500

    stubbing out aliases in user model; adding ls_comment model, controller, and views

--- app/models/l_story.rb
+++ app/models/l_story.rb
@@ -1,4 +1,5 @@
 class LStory < ApplicationRecord
+    # has_many :users NOTE: This is for an API wherein many users can add to the same textarea; now usrs made a story through comments
     belongs_to :user
     has_many :l_story_o_genres
     has_many :o_genres, through: :l_story_o_genres
