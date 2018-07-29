c5593346ec74c1d63fb323dbe5a888ad7efe362b
github username <github email address>
2018-07-29 11:02:07 -0500

    built routes and associatiosn for fav_lit feature

--- app/models/fav_lit.rb
+++ app/models/fav_lit.rb
@@ -1,2 +1,13 @@
 class FavLit < ApplicationRecord
-end
+    belongs_to :fav_author
+    has_many :fav_lit_fav_genres    
+    has_many :fav_genres, through: :fav_lit_fav_genres
+    accepts_nested_attributes_for :fav_genres
+
+    # def categories_attributes=(category_attributes)
+    #     category_attributes.values.each do |category_attribute|
+    #       category = Category.find_or_create_by(category_attribute)
+    #       self.categories << category
+    #     end 
+    # end
+end 
