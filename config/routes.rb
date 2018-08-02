8f87234e26743b94206f6819f23bcce7aadbd867
github username <github email address>
2018-08-02 16:37:10 -0500

    added controllers, views, and routes for fanfave/fan clubs

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
 
   get 'sourcer_projects', to: 'static#sourcer_projects'
   get 'living_stories', to: 'static#living_stories'
+  get 'all_sf_fanvorites', to: 'static#all_sf_fanvorites'
+  get 'sf_lit_fanvorites', to: 'static#sf_lit_fanvorites'
+  get 'sf_film_fanvorites', to: 'static#sf_film_fanvorites'
+  get 'all_game_fanvorites', to: 'static#all_game_fanvorites'
+  get 'all_fan_pages', to: 'static#all_fan_pages'
 
-
-  
   namespace :admin do
     resources :funding_categories, only: [:new, :create, :edit, :update, :destroy]
   end
