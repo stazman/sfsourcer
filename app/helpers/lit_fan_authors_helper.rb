0c5dcdc34eefb6766e47789d5ea28334feaf2c8c
github username <github email address>
2018-08-03 12:23:50 -0500

    building lit_fan_works#index view and helper methods

--- app/helpers/lit_fan_authors_helper.rb
+++ app/helpers/lit_fan_authors_helper.rb
@@ -1,2 +1,13 @@
 module LitFanAuthorsHelper
+    def display_author(lit_fan_work)
+        lit_fan_work.lit_fan_author.nil? ? link_to("Add Author", edit_lit_fan_work_path(lit_fan_work)) : link_to(lit_fan_work.lit_fan_author_name, lit_fan_author_path(lit_fan_work.lit_fan_author))
+      end
+    
+    def lit_fan_author_select(lit_fan_author, lit_fan_work)
+        if lit_fan_author
+            lit_fan_author.name
+        else
+            select_tag "lit_fan_work[lit_fan_author_id]", options_from_collection_for_select(LitFanAuthor.all, :id, :name)
+        end
+    end
 end
