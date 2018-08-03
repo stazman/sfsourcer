b57e518684b587a43eb9bd6921218ed5b58fcab7
github username <github email address>
2018-08-03 14:05:02 -0500

    created seeds for lit_fan_work, _author, and _genre

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+work1 = LitFanWork.create(id: 1, title: "Dune", lit_fan_author_id: 1)
+author1 = LitFanAuthor.create(id: 1, name: "Frank Herbert")
+genre1 = LitFanGenre.create(id: 1, name: "Classic Science Fiction")
