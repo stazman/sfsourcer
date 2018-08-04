be0f245db0ed05f69d7bf06b5495204f425d07f8
github username <github email address>
2018-08-04 10:53:06 -0500

    getting new author to instantiate correctly with new work

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,6 @@ a = User.create(name: "a", email: "a@a.com", password: "a")
 # frank_jones_cats_pledge = Pledge.create(amount: 10, user_id: 1, funding_project_id: 1)
 aa = Address.create(address1: "100 Main Street", city: "Springfield", state: "Ohio", zip: "45454", user_id: 1)
 
-work1 = LitFanWork.create(id: 1, title: "Dune")
+work1 = LitFanWork.create(title: "Dune")
 author1 = LitFanAuthor.create(name: "Frank Herbert")
-genre1 = LitFanGenre.create(id: 1, name: "Classic Science Fiction")
+genre1 = LitFanGenre.create(name: "Classic Science Fiction")
