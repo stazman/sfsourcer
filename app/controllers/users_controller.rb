0693ce9f27405a7b8d16459247b2afb9a7db6e27
github username <github email address>
2018-08-06 23:43:00 -0500

    moved files for features under construction to specific folder

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   def create  
     @user = User.new(user_params)
     if @user.valid?
+      @user.save
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
