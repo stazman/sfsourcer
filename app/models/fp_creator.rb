da06bc92c57ae276ae5f298294eb2f60d75b67c0
github username <github email address>
2018-08-07 19:39:49 -0500

    getting fp_creator to instantiate as a nested resource to user

--- app/models/fp_creator.rb
+++ app/models/fp_creator.rb
@@ -1,4 +1,13 @@
 class FpCreator < ApplicationRecord
     belongs_to :user
     has_many :funding_projects
+
+    def fpc_user_name
+        self.try(:user).try(:name)
+    end
+    # but how does this work from just the parameters??? what mechanism takes what instance through the strong params and how does it do it?
+    def fpc_user_name=(name)
+        fpc_user = User.find_or_create_by(name: name)
+        self.fpc_user = fpc_user
+    end
 end
