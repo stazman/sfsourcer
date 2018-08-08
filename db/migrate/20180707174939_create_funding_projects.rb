b13207bb7fbd5546bd2ac60f0eb4772b64fb6c9d
github username <github email address>
2018-08-08 10:16:08 -0500

    finished migrations for fp_creator/funding_projects

--- db/migrate/20180707174939_create_funding_projects.rb
+++ db/migrate/20180707174939_create_funding_projects.rb
@@ -1,11 +1,10 @@
 class CreateFundingProjects < ActiveRecord::Migration[5.2]
   def change
     create_table :funding_projects do |t|
-      t.string :title
       t.string :creator_name
+      t.string :title
       t.string :description
       t.integer :funding_goal
-      t.integer :user_id
       t.string :created_at
       t.string :updated_at
     
