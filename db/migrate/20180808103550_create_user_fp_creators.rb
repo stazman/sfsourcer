8993e71e4e9497b26ce571d9e49282a8f700e2eb
github username <github email address>
2018-08-08 11:17:38 -0500

    changed seed for funding_project/fp_creator, migrated

--- db/migrate/20180808103550_create_user_fp_creators.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserFpCreators < ActiveRecord::Migration[5.2]
-  def change
-    create_table :user_fp_creators do |t|
-      t.integer :user_id
-      t.integer :fp_creator_id
-      t.timestamps
-    end
-  end
-end
