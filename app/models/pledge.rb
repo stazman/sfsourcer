d6aabbb8202bd36b9190fca981fcc53847cdb599
github username <github email address>
2018-08-09 16:41:51 -0500

    added pledges_attributes writer to pledge model for fp_backed

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -6,6 +6,11 @@ class Pledge < ApplicationRecord
     # def funding_project_title=(funding_project_title)
     #     self.funding_project.title = FundingProject.find_by(title: funding_project_title)
     # end
+    def pledges_attributes=(pledges_attributes)
+        pledges_attributes.values.each do |pledge_attribute|
+        p = Pledge.find_by(pledge_attribute)
+        self.fp_backeds << p
+    end
 
     validates_presence_of :amount
     validates :amount, numericality: true
