fb23b688008e41aab4e0fa32eaaaf15b718c0b73
github username <github email address>
2018-08-09 15:44:49 -0500

    created fp_backed, made has many through funding_projects/pledges; stubbed for fp_backer/funding_project/pledge

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -1,10 +1,10 @@
 class Pledge < ApplicationRecord
-    belongs_to :user
+    belongs_to :fp_backed
     belongs_to :funding_project
-    
-    def funding_project_title=(funding_project_title)
-        self.funding_project.title = FundingProject.find_by(title: funding_project_title)
-    end
+   
+    # def funding_project_title=(funding_project_title)
+    #     self.funding_project.title = FundingProject.find_by(title: funding_project_title)
+    # end
 
     validates_presence_of :amount
     validates :amount, numericality: true
