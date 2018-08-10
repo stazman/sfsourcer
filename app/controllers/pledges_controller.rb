5ecc55f82b694ffa159f14e00b9bc8f214c6ea1f
github username <github email address>
2018-08-10 13:44:50 -0500

    returned pledges controller and views to before fp_backed

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -1,43 +1,50 @@
 class PledgesController < ApplicationController
     before_action :require_login
-    
+  
     def index
-        @pledges = Pledge.all
+      @pledges = Pledge.all
     end
-
+  
     def new
-        @pledge = Pledge.new(fp_backed_id: params[:fp_backed_id])
+      @pledge = Pledge.new
     end
-
+  
     def create
-        @pledge = Pledge.new(pledge_params)
+      @pledge = Pledge.new(pledge_params)
+      @pledge.user_id = current_user.id
+      @pledge.funding_project_id = params[:pledge][:funding_project_id] 
+      if @pledge.valid?
         @pledge.save
-        redirect_to @pledge
+        redirect_to pledge_path(@pledge)
+      else
+        render :new
+      end
     end
         
-
+  
     def show
+      @pledge = Pledge.find(params[:id])
     end
-
+  
     def edit
     end
-
+  
     def update
     end
-
+  
     def destroy
     end
-
-    private
-
+  
+  private
+  
     def pledge_params
-        params.require(:pledge).permit(:amount)
+      params.require(:pledge).permit(:amount, :funding_project_title, :user_id, :funding_project_id)
     end
-
+  
     def require_login
-        unless logged_in?
+      unless logged_in?
         flash[:alert] = "You must be logged in to access this section"
         redirect_to login_path
-        end
+      end
     end
-end
+  end
