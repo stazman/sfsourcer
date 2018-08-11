b5eda8ee6b6a7238746351680c619e821aa17385
github username <github email address>
2018-08-11 13:36:06 -0500

    debugging new pledge not instantiating

--- app/models/fp_backer.rb
+++ app/models/fp_backer.rb
@@ -1,6 +1,7 @@
 class FpBacker < ApplicationRecord
     has_many :pledges
     has_many :funding_projects, through: :pledges
+    has_many :funding_projects
     # has_many :user_fp_backers
     # has_many :users, through: :user_fp_backers
     # this may be better as a has many but not has many through, maybe depending on polymorphic with only other classes involved such as comments
