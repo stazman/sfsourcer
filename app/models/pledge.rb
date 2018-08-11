f5eb789f79dbe7295283d45989631a7089b7ff5d
github username <github email address>
2018-08-11 12:42:38 -0500

    removed pledges as a nested resource for funding_projects; returning to abstract querying

--- app/models/pledge.rb
+++ app/models/pledge.rb
@@ -5,18 +5,21 @@ class Pledge < ApplicationRecord
     validates_presence_of :amount
     validates :amount, numericality: true
 
-    def funding_project_name
-        self.try(:funding_project).try(:name)
-    end
-
-    def funding_project_name=(name)
-        fp = FundingProject.find_by(name: name)
-        self.funding_project = fp
+    def funding_project_title=(funding_project_title)
+        self.funding_project.title = FundingProject.find_by(title: funding_project_title)
     end
 end
 
 
 
+# def funding_project_name
+#     self.try(:funding_project).try(:name)
+# end
+
+# def funding_project_name=(name)
+#     fp = FundingProject.find_by(name: name)
+#     self.funding_project = fp
+# end
 
     # def fp_backed_name
     #     self.try(:fp_backed).try(:name)
@@ -27,6 +30,3 @@ end
     #     self.fp_backed = fpb
     # end
 
-    # # def funding_project_title=(funding_project_title)
-    # #     self.funding_project.title = FundingProject.find_by(title: funding_project_title)
-    # # end
