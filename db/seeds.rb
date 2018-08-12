140e33ea1015d1f8ab34589bf9555c574e72ac5b
github username <github email address>
2018-08-11 23:36:54 -0500

    getting pledge id to instantiate

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,12 @@
-a = User.create!(name: "a", email: "a@a.com", password: "a")
-b_fp = FundingProject.create!(title: "Test Fp", description: "test", funding_goal: "200")
-a_fpcreator = FpCreator.create!(creator_name: "ac", location: "tampa")
-# a_pledge = Pledge.create(amount: 20)
-a_backer = FpBacker.create!(name: "a", backer_email: "a@a.com")
+# a = User.create!(name: "a", email: "a@a.com", password: "a")
+# b_fp = FundingProject.create!(title: "Test Fp", description: "test", funding_goal: "200")
+# a_fpcreator = FpCreator.create!(creator_name: "ac", location: "tampa")
+# # a_pledge = Pledge.create(amount: 20)
+# a_backer = FpBacker.create!(name: "a", backer_email: "a@a.com")
+b_pledge = Pledge.create!(amount: 10, funding_project_id: 1, fp_backer_id: 1)
 
-author1 = LitFanAuthor.create(name: "Frank Herbert")
-genre1 = LitFanGenre.create!(name: "Classic Science Fiction")
-work1 = LitFanWork.create!(title: "Dune")
+# author1 = LitFanAuthor.create(name: "Frank Herbert")
+# genre1 = LitFanGenre.create!(name: "Classic Science Fiction")
+# work1 = LitFanWork.create!(title: "Dune")
 
 
