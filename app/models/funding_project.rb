f1c195bc3c3fe6d6a53038010253f405e5e9da48
github username <github email address>
2018-08-13 13:48:19 -0500

    changed associations to make all join tables between fp_backer/funding_projects/pledges

--- app/models/funding_project.rb
+++ app/models/funding_project.rb
@@ -3,9 +3,10 @@ class FundingProject < ApplicationRecord
     has_many :funding_project_fp_creators
     has_many :fp_creators, through: :funding_project_fp_creators
     accepts_nested_attributes_for :fp_creators
-    has_many :pledge_funding_projects
+    has_many :fp_backer_funding_projects
+    has_many :fp_backers, through: :fp_backer_funding_projects
+    has_many :funding_project_pledges
     has_many :pledges, through: :pledge_funding_projects
-    # has_many :fp_backers, through: :pledges
 
     validates_presence_of :title
     validates_uniqueness_of :title
