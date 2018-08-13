bb33958074e8026dfb222681abbdd8c941622ca2
github username <github email address>
2018-08-13 14:29:21 -0500

    git test on branch to be deleted

--- app/models/funding_project.rb
+++ app/models/funding_project.rb
@@ -1,5 +1,50 @@
 class FundingProject < ApplicationRecord
       
+    class Pledge < ApplicationRecord
+        has_many :funding_project_pledges
+        has_many :funding_projects, through: :funding_project_pledges
+        has_many :fp_backer_pledges
+        has_many :fp_backers, through: :fp_backer_pledges
+           
+        # validates_presence_of :amount
+        # validates :amount, numericality: true
+    
+        # def funding_project_title=(funding_project_title)
+        #     self.funding_project.title = FundingProject.find_by(title: funding_project_title)
+        # end
+        def fp_backer_name
+            self.try(:fp_backer).try(:name)
+        end
+    
+        def fp_backer_name=(name)
+            fpb = FpBacker.find_or_create_by(name: name)
+            self.fp_backer = fpb
+        end
+    
+        def funding_project_attributes=(funding_project_attributes)
+            funding_project_attributes.values.each do |fp_attribute|
+            fp = FundingProject.find(fp_attribute) 
+            self.funding_projects << fp
+            end
+        end 
+    end
+    
+    
+    
+    # def funding_project_name
+    #     self.try(:funding_project).try(:name)
+    # end
+    
+    
+        # def fp_backed_name
+        #     self.try(:fp_backed).try(:name)
+        # end
+        # # but how does this work from just the parameters??? what mechanism takes what instance through the strong params and how does it do it?
+        # def fp_backed_name=(name)
+        #     fpb = FpBacked.find_or_create_by(name: name)
+        #     self.fp_backed = fpb
+        # end
+    
     has_many :funding_project_fp_creators
     has_many :fp_creators, through: :funding_project_fp_creators
     accepts_nested_attributes_for :fp_creators
