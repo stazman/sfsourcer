fe6770876b5e8c539db72439dbb20c58c60a0f54
github username <github email address>
2018-08-16 22:00:40 -0500

    created route

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   get '/sf_game_fanvorites', to: 'static#sf_game_fanvorites'
   get '/sourcer_projects', to: 'static#sourcer_projects'
 
+  get '/lit_fan_authors/author_most_works', to: 'lit_fan_authors#author_most_works'
+
   resources :users
   resources :funding_projects
   resources :fp_creators
