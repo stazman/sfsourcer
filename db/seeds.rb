78788f1e2a1aab9590c05ceacefff49c8a0db4e2
github username <github email address>
2018-08-18 15:55:17 -0500

    adding links for new author

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,6 @@ a = User.create!(name: "a", email: "a@a.com", password: "a")
 # # a_backer = FpBacker.create!(name: "a", backer_email: "a@a.com")
 # b_pledge = Pledge.create!(amount: 10, funding_project_id: 1, fp_backer_id: 1)
 
-author1 = LitFanAuthor.create(name: "Frank Herbert")
-genre1 = LitFanGenre.create(name: "Classic Science Fiction")
-work1 = LitFanWork.create(title: "Dune")
-
-author1 = LitFanAuthor.create(name: "Isaac Asimov")
-genre1 = LitFanGenre.create(name: "Classic Science Fiction")
-work1 = LitFanWork.create(title: "I, Robot")
-
-author1 = LitFanAuthor.create(name: "William Gibson")
-genre1 = LitFanGenre.create(name: "Cyber Punk")
-work1 = LitFanWork.create(title: "Neuromancer")
-
+# author1 = LitFanAuthor.create!(name: "William Gibson")
+# work1 = LitFanWork.create!(title: "Dune")
 
