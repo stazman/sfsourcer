273bc48bdf746f2ab0d38dd457d6dd2f0f1ea7f6
github username <github email address>
2018-08-18 23:48:41 -0500

    added all reasonable validations to user/fanvorites features

--- app/models/lit_fan_author.rb
+++ app/models/lit_fan_author.rb
@@ -1,6 +1,9 @@
 class LitFanAuthor < ApplicationRecord
     has_many :lit_fan_works
 
+    validates_presence_of :lit_fan_work
+    validates_uniqueness_of :lit_fan_work
+    
     def self.search(term)
         if term
           where('name LIKE ?', "%#{term}%")
