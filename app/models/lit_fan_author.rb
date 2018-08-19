4451d0f8bed036a0259a9969bde683f628fef49e
github username <github email address>
2018-08-19 00:20:39 -0500

    edited/tested user validations

--- app/models/lit_fan_author.rb
+++ app/models/lit_fan_author.rb
@@ -14,17 +14,5 @@ class LitFanAuthor < ApplicationRecord
 
     def self.alphabetized
       all.order(name: :asc)
-
-      # joins(:lit_fan_works).group("lit_fan_author.id").order(title: :desc).limit(1)
-      # binding.pry
-      
-      # having("COUNT(*) = 3").select("boats.*")
-      # select authors where there are works
-      # order the authors by most works asc limit 1 
     end
-
-
-    # def self.most_works
-    #   self.lit_fan_works.sort.last
-    # end
 end
