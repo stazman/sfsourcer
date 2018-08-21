27299df310d21aabbd2729413597c457ee933b83
CHRISTOPHER DISTASIO <christopher_distasio@CHRISTOPHERs-MacBook-Air-1.local>
2018-08-21 13:56:19 -0500

    lit_fan_author/_works not instantiating with seeds: fixed

--- app/models/lit_fan_author.rb
+++ app/models/lit_fan_author.rb
@@ -1,8 +1,8 @@
 class LitFanAuthor < ApplicationRecord
     has_many :lit_fan_works
 
-    validates_presence_of :lit_fan_author
-    validates_uniqueness_of :lit_fan_author
+    # validates_presence_of :lit_fan_author
+    # validates_uniqueness_of :lit_fan_author
     
     def self.search(term)
         if term
