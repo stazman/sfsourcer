a105993dbff5a8168a082e1289f83aa5b2bf26c7
CHRISTOPHER DISTASIO <christopher_distasio@CHRISTOPHERs-MacBook-Air-1.local>
2018-08-21 13:27:10 -0500

    removed/stored migrations irrelevant to current project

--- db/migrate/20180707170145_create_users.rb
+++ db/migrate/20180707170145_create_users.rb
@@ -5,6 +5,9 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :email
       t.string :password
       t.string :password_digest
+      t.string :fav_lits
+      t.string :fav_films
+      t.string :fav_games
       t.string :created_at
       t.string :updated_at
       t.timestamps
