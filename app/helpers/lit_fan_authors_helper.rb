468e2666a49f3846b34ffae9c78403bd460b3829
CHRISTOPHER DISTASIO <christopher_distasio@CHRISTOPHERs-MacBook-Air-1.local>
2018-08-21 14:20:03 -0500

    edting now functions for all fanvorite models, through lit_fan_works only

--- app/helpers/lit_fan_authors_helper.rb
+++ app/helpers/lit_fan_authors_helper.rb
@@ -1,7 +1,7 @@
 module LitFanAuthorsHelper
-    # def display_author(lit_fan_work)
-    #     lit_fan_work.lit_fan_author.nil? ? link_to("Add Author", edit_lit_fan_work_path(lit_fan_work)) : link_to(lit_fan_work.lit_fan_author_name, lit_fan_author_path(lit_fan_work.lit_fan_author))
-    #   end
+    def display_author(lit_fan_work)
+        lit_fan_work.lit_fan_author.nil? ? link_to("Add Author", edit_lit_fan_work_path(lit_fan_work)) : link_to(lit_fan_work.lit_fan_author_name, lit_fan_author_path(lit_fan_work.lit_fan_author))
+      end
     
     # def lit_fan_author_select(lit_fan_author, lit_fan_work)
     #     if lit_fan_author
