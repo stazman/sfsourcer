157d180aa681a9192f89d15e1e91fbd914096a17
CHRISTOPHER DISTASIO <christopher_distasio@CHRISTOPHERs-MacBook-Air-1.local>
2018-08-21 18:47:32 -0500

    debugged edit going to wrong user issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,7 +24,12 @@ class SessionsController < ApplicationController
     end
 
     def destroy
-        session.delete :user_id
-        render :new
+        reset_session
+        session[:user_id] = nil
+        #  if session[:user_id]
+        # @current_user = nil
+        # session.delete :user_id
+        # redirect_to login_path
+        redirect_to login_path
     end
 end
