8887b83568443b059b7a205f8013c0e1eeb43338
CHRISTOPHER DISTASIO <christopher_distasio@CHRISTOPHERs-MacBook-Air-1.local>
2018-08-23 12:21:01 -0500

    Facebook signup/signin works; user found or created

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Sfsourcer
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.force_ssl = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
