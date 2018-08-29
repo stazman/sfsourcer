5ab7fd826b59bfac5cd3ea4deb86b15292801561
stazman <cadistasio@gmail.com>
2018-08-28 23:37:13 -0500

    cleaned up code/comments

--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,10 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   get 'auth/facebook/callback', to: 'sessions#create'
   get 'auth/failure', to: 'static#home'
-  # get '/sessions', to: 'sessions#create'
-  # post '/sessions', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
-  # get '/auth/facebook/callback' => 'sessions#create'
-
+ 
   get '/all_sf_fanvorites', to: 'static#all_sf_fanvorites'
   get '/sf_lit_fanvorites', to: 'static#sf_lit_fanvorites'
-  # get '/sf_film_fanvorites', to: 'static#sf_film_fanvorites'
-  # get '/sf_game_fanvorites', to: 'static#sf_game_fanvorites'
-  # get '/sourcer_projects', to: 'static#sourcer_projects'
-
   get '/lit_fan_authors/alphabetized', to: 'lit_fan_authors#alphabetized'
 
   resources :users
@@ -29,14 +22,3 @@ Rails.application.routes.draw do
     resources :lit_fan_works, only: [:index, :new, :show, :edit]
   end
 end
-
-  # resources :funding_projects
-  # resources :fp_creators
-  # resources :pledges
-  # resources :fp_backers
-  # resources :sf_favs
-  
-  # resources :users do
-  #   resources :sf_favs, only: [:new, :show, :edit]
-  # end
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
 b = User.create(name: "a", email: "a@a.com", password: "Aa123!")
+
+author1 = LitFanAuthor.create!(name: "Frank Herbert")
+
+
 # # b_fp = FundingProject.create!(title: "Test Fp", description: "test", funding_goal: "200")
 # # a_fpcreator = FpCreator.create!(creator_name: "ac", location: "tampa")
 # # # a_pledge = Pledge.create(amount: 20)
 # # a_backer = FpBacker.create!(name: "a", backer_email: "a@a.com")
 # b_pledge = Pledge.create!(amount: 10, funding_project_id: 1, fp_backer_id: 1)
 
-author1 = LitFanAuthor.create!(name: "Frank Herbert")
 # work1 = LitFanWork.create!(title: "Dune")
 # genre1 = LitFanGenre.create!(name: "Science Fiction")
 
