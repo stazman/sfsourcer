35be24f302017d4749869332d5c4c0a465c1870e
stazman <cadistasio@gmail.com>
2018-09-05 17:38:46 -0500

    changed associations

--- app/models/l_story.rb
+++ app/models/l_story.rb
@@ -2,7 +2,7 @@ class LStory < ApplicationRecord
     # has_many :users NOTE: This is for an API wherein many users can add to the same textarea; now usrs made a story through comments
     # belongs_to :user
     has_many :ls_comments
-    has_many :users, through: :ls_comments
+    has_many :ls_users, through: :ls_comments
       # has_many :l_story_o_genres
     # has_many :ls_genres, through: :l_story_o_genres
     # accepts_nested_attributes_for :ls_genres
--- app/models/ls_comment.rb
+++ app/models/ls_comment.rb
@@ -1,6 +1,5 @@
 class LsComment < ApplicationRecord
     belongs_to :l_story
-    belongs_to :user
-    accepts_nested_attributes_for :user
-    # , :reject_if => :all_blank
+    belongs_to :ls_user
+    accepts_nested_attributes_for :ls_user, :reject_if => :all_blank
 end
