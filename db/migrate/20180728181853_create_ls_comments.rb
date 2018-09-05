b62a7db2acdc21bd2ca55ddb0f0f14647b2283cc
stazman <cadistasio@gmail.com>
2018-09-05 12:12:03 -0500

    added ls_comments table; migrated

--- db/migrate/20180728181853_create_ls_comments.rb
+++ db/migrate/20180728181853_create_ls_comments.rb
@@ -2,7 +2,8 @@ class CreateLsComments < ActiveRecord::Migration[5.2]
   def change
     create_table :ls_comments do |t|
       t.string :content
-      t.string :user_id
+      t.integer :user_id
+      t.integer :l_story_id
 
       t.timestamps
     end
