3cad905fa21cea90c68dba1668cd25e4e7df221a
stazman <cadistasio@gmail.com>
2018-09-06 17:21:34 -0500

    allow user to log in through Facebook

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,13 @@ class SessionsController < ApplicationController
 
   def create
     # # raise params.inspect
-    # if request.env['omniauth.auth']
-    #   @user = User.find_or_create_with_omniauth(request.env['omniauth.auth'])
-    #   session[:user_id] = @user.id
-    #   @current_user = session[:user_id]
-    #   redirect_to root_url
-    # #   user_path(user.id)( ??? shouldn't this work if the app is working, or it doesn't make sense because user info is stored on FB db?)
-    # else
+    if request.env['omniauth.auth']
+      @user = User.find_or_create_with_omniauth(request.env['omniauth.auth'])
+      session[:user_id] = @user.id
+      @current_user = session[:user_id]
+      redirect_to root_url
+    #   user_path(user.id)( ??? shouldn't this work if the app is working, or it doesn't make sense because user info is stored on FB db?)
+    else
       user = User.find_by(email: params[:sessions][:email])
       # binding.pry
       # if user && user.authenticate(params[:session][:password])      
@@ -20,14 +20,14 @@ class SessionsController < ApplicationController
       # user && user.authenticate(params[:password])
         session[:user_id] = user.id
       # else
-            redirect_to root_url
+        redirect_to root_url
 
         # render :new
       # unless session[:user_id] == user.id
       #   flash[:notice] = "You don't have access to that order!"
       #   redirect_to user_path(@user)
       # end
-    # end
+    end
   end
     
   def destroy
