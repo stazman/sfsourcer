a20f32b8a893b19ea57ad6cb70e5fd2c31bdd1a4
stazman <cadistasio@gmail.com>
2019-01-18 22:58:58 -0600

    adds record added alert to main js/attempts unsuccessfully to fix features dropdown not available when landing on page issue

--- app/assets/javascripts/lit_fan_works.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-    $(a."record_added").on("click", function(e) {
-    alert("Record added!")
-    e.preventDefault();
-    })
-})
---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,5 @@
+$('body').on('click', '.dropdown-toggle', function () { $(this).dropdown(); });
+
+function recordAdded() {
+    alert("Record added!");
+}
