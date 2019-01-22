3968539e9e221cda70456507b9c32ff1cb592649
stazman <cadistasio@gmail.com>
2019-01-22 17:50:43 -0600

    moves asset pipeline code into shim

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,3 +16,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( main.css )
 
 Rails.application.config.assets.precompile += %w( main.js )
+
+Rails.application.config.assets.precompile += %w( funding_projects.js )
