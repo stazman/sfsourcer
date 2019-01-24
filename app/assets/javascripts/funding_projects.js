033943a6ee0de834dcdefb457842db68af9572a7
stazman <cadistasio@gmail.com>
2019-01-24 12:14:25 -0600

    removes turbolinks from pipeline so that new fp can be made with js file (nonworking features) added to layouts

--- app/assets/javascripts/funding_projects.js
+++ app/assets/javascripts/funding_projects.js
@@ -2,3 +2,26 @@ $('a.load_fps').on('click', function(e) {
     alert("Here are all the projects!");
     e.preventDefault();
 })
+
+// $(function () {
+//   $(".js-more").on('click', function() {
+//     // get the id from the data attribute
+//     let id = $(this).data("id");
+//     $.get("/funding_projects/" + id + "/body", function(data) {
+//       alert(data);
+//     });
+//   });
+// });
+
+
+$(function () {
+    $(".js-more").on('click', function() {
+      let id = $(this).data("id");
+      $.get("/funding_projects/" + id + "/body", function(data) {
+        // Replace text of body-id div
+        $("#body-" + id).text(data);
+      });
+    });
+  });
+
+
