f2446ec206d5a66e8f3673f106af862acd36f7c3
stazman <cadistasio@gmail.com>
2019-01-30 15:53:10 -0600

    creates alert upon creation of fp

--- app/assets/javascripts/funding_projects.js
+++ app/assets/javascripts/funding_projects.js
@@ -1,17 +1,17 @@
-$('a.load_fps').on('click', function(e) {
-    alert("Here are all the projects!");
+$(function(){
+  $("form#new_funding_project").on("submit", function(e){
+    alert("You've created a new project!");
     e.preventDefault();
+
+    let $form = $(this);
+    let action = $form.attr("action");
+    let params = $form.serialize()
+
+    $.post(action, params)
+    // console.log("A Project is Born!");
+  })
 })
 
-// $(function () {
-//   $(".js-more").on('click', function() {
-//     // get the id from the data attribute
-//     let id = $(this).data("id");
-//     $.get("/funding_projects/" + id + "/body", function(data) {
-//       alert(data);
-//     });
-//   });
-// });
 
 
 $(function () {
@@ -24,4 +24,19 @@ $(function () {
     });
   });
 
+
+// $('a.load_fps').on('click', function(e) {
+//     alert("Here are all the projects!");
+//     e.preventDefault();
+// })
+
+// $(function () {
+//   $(".js-more").on('click', function() {
+//     // get the id from the data attribute
+//     let id = $(this).data("id");
+//     $.get("/funding_projects/" + id + "/body", function(data) {
+//       alert(data);
+//     });
+//   });
+// });
 
