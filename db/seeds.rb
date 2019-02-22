092322cd1eb6593586eb8e0e9ed2b54695028f88
stazman <cadistasio@gmail.com>
2019-02-22 16:50:04 -0600

    completes, migrates, seeds db for writer_group

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
 
-author_1 = LitFanAuthor.create!(name: "Frank Herbert")
+# author_1 = LitFanAuthor.create!(name: "Frank Herbert")
 
-ls_user = LsUser.create!(username: "Funky Fred")
-ls_1 = LStory.create!(title: "From the Basement", content: "I heard a noise from the basement")
-ls_2 = LStory.create!(title: "You, Robot", content: "Why is there a robot eating my foot, I wondered")
-lsc_1 = LsComment.create!(content: "ghost!", ls_user_id: 1, l_story_id: 1)
+# ls_user = LsUser.create!(username: "Funky Fred")
+# ls_1 = LStory.create!(title: "From the Basement", content: "I heard a noise from the basement")
+# ls_2 = LStory.create!(title: "You, Robot", content: "Why is there a robot eating my foot, I wondered")
+# lsc_1 = LsComment.create!(content: "ghost!", ls_user_id: 1, l_story_id: 1)
+wg_1 = WriterGroup.create!(wg_creator: "Christopher DiStasio", wg_name: "Science-Fantasy", wg_description: "We look forward to helping each other improve our writing in a friendly, productive environment. The work of writers in this section tend to have elements of both hard science fiction and fantasy.")
 # # b_fp = FundingProject.create!(title: "Test Fp", description: "test", funding_goal: "200")
 # # a_fpcreator = FpCreator.create!(creator_name: "ac", location: "tampa")
 # # # a_pledge = Pledge.create(amount: 20)
