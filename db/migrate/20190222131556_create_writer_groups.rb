a24ecef57f4b789384538cea67f975ca7b69a71c
stazman <cadistasio@gmail.com>
2019-02-22 16:50:04 -0600

    completes, migrates, seeds db for writer_group

--- db/migrate/20190222131556_create_writer_groups.rb
+++ db/migrate/20190222131556_create_writer_groups.rb
@@ -1,6 +1,9 @@
 class CreateWriterGroups < ActiveRecord::Migration[5.2]
   def change
     create_table :writer_groups do |t|
+      t.string :wg_creator
+      t.string :wg_name
+      t.text :wg_description
 
       t.timestamps
     end
