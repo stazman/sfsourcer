5891f588f5cedef956ac173427ee2e93444f5217
stazman <cadistasio@gmail.com>
2019-02-22 16:32:19 -0600

    adds functionality for writer_group controller views new create show

--- app/controllers/writer_groups_controller.rb
+++ app/controllers/writer_groups_controller.rb
@@ -1,14 +1,23 @@
 class WriterGroupsController < ApplicationController
   def index
+    @writer_groups = WriterGroup.all
   end
 
   def new
+    @writer_group = WriterGroup.new
   end
 
   def create
+    @writer_group = WriterGroup.create(writer_group_params)
+    if @funding_project.save
+      redirect_to @writer_group
+    else
+      render :new
+    end
   end
 
   def show
+    @writer_group = WriterGroup.find(params[:id])
   end
 
   def edit
@@ -19,4 +28,11 @@ class WriterGroupsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def writer_group_params
+    params.require(:writer_group).permit(:wg_creator, :wg_name, :wg_description)
+  end
+
 end
