1f944a9eabc5fde72cf3d9c3360d24863b0446e1
stazman <cadistasio@gmail.com>
2019-02-24 02:49:10 -0600

    sets up wg_comments index, new, create, show actions

--- app/controllers/wg_comments_controller.rb
+++ app/controllers/wg_comments_controller.rb
@@ -1,14 +1,23 @@
 class WgCommentsController < ApplicationController
   def index
+    @wg_comments = WgComment.all
   end
 
   def new
+    @wg_comment = WgComment.new
   end
 
   def create
+    @wg_comment = WgComment.create(wg_comment_params)
+    if @wg_comment.save
+      redirect_to @wg_comment
+    else
+      render :new
+    end
   end
 
   def show
+    @wg_comment = WgComment.find(params[:id])   
   end
 
   def edit
@@ -18,5 +27,11 @@ class WgCommentsController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+
+  def wg_comment_params
+    params.require(:wg_comment).permit(:author, :content)
   end
 end
