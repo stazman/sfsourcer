9d6c009de465374b32f204a57a68b545c5674637
stazman <cadistasio@gmail.com>
2019-02-23 23:41:23 -0600

    removes duplicate entry from writer_groups

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 # ls_1 = LStory.create!(title: "From the Basement", content: "I heard a noise from the basement")
 # ls_2 = LStory.create!(title: "You, Robot", content: "Why is there a robot eating my foot, I wondered")
 # lsc_1 = LsComment.create!(content: "ghost!", ls_user_id: 1, l_story_id: 1)
-wg_1 = WriterGroup.create!(wg_creator: "Christopher DiStasio", wg_name: "Science-Fantasy", wg_description: "We look forward to helping each other improve our writing in a friendly, productive environment. The work of writers in this section tend to have elements of both hard science fiction and fantasy.")
+# wg_1 = WriterGroup.create!(wg_creator: "Christopher DiStasio", wg_name: "Science-Fantasy", wg_description: "We look forward to helping each other improve our writing in a friendly, productive environment. The work of writers in this section tend to have elements of both hard science fiction and fantasy.")
 # # b_fp = FundingProject.create!(title: "Test Fp", description: "test", funding_goal: "200")
 # # a_fpcreator = FpCreator.create!(creator_name: "ac", location: "tampa")
 # # # a_pledge = Pledge.create(amount: 20)
@@ -15,3 +15,4 @@ wg_1 = WriterGroup.create!(wg_creator: "Christopher DiStasio", wg_name: "Science
 # work1 = LitFanWork.create!(title: "Dune")
 # genre1 = LitFanGenre.create!(name: "Science Fiction")
 
+DELETE FROM writer_groups WHERE ID = 3;
