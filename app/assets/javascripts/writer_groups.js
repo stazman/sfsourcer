679f71aa581c88fcc4a3e5a861969df0ba381aa1
stazman <cadistasio@gmail.com>
2019-02-24 17:42:50 -0600

    Creates links from index list items to render writer_groups show pages through js and AMS

--- app/assets/javascripts/writer_groups.js
+++ app/assets/javascripts/writer_groups.js
@@ -1,16 +1,12 @@
 $(function(){
     $("a.load_writer_groups").on("click", function(e){
-        // alert("This is a test");
         $.get(this.href).success(function(json){
-            // console.log(data);
-            // $("body").append(data);
-            // $("div.writer_groups.ul").
+
             let $ul = $("div.writer_groups ul")
 
             $ul.html("");
-
             json.forEach(function(writer_group){
-                $ul.append("<li>" + writer_group.wg_name + " - " + writer_group.wg_creator + "</li>");
+                $ul.append("<li>" + "<a " + "href='" + "/writer_groups/" + writer_group.id + "' >" + writer_group.wg_name + "</a>" + " - " + writer_group.wg_creator + "</li>");
             });
         });
         e.preventDefault();
