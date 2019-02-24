2b618337260219eaf098074fac892f23be9ed6a2
stazman <cadistasio@gmail.com>
2019-02-24 02:44:54 -0600

    sets up wg_works index, new, create, show actions

--- app/controllers/wg_works_controller.rb
+++ app/controllers/wg_works_controller.rb
@@ -1,14 +1,23 @@
 class WgWorksController < ApplicationController
   def index
+    @wg_works = WgWork.all
   end
 
   def new
+    @wg_work = WgWork.new
   end
 
   def create
+    @wg_work = WgWork.create(wg_work_params)
+    if @wg_work.save
+      redirect_to @wg_work
+    else
+      render :new
+    end
   end
 
   def show
+    @wg_work = WgWork.find(params[:id])   
   end
 
   def edit
@@ -18,5 +27,11 @@ class WgWorksController < ApplicationController
   end
 
   def delete
+  end
+
+  private
+
+  def wg_work_params
+    params.require(:wg_work).permit(:author, :bio, :title, :version, :content)
   end
 end
