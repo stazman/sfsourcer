3dc0d6dfa752df87c97d92ebf5c9fe929b801698
stazman <cadistasio@gmail.com>
2019-02-26 11:52:42 -0600

    removes unneeded files/associations for R/JS Project

--- app/controllers/wg_works_controller.rb
+++ app/controllers/wg_works_controller.rb
@@ -1,10 +1,10 @@
 class WgWorksController < ApplicationController
   def index
     @wg_works = WgWork.all
-    respond_to do |f|
-      f.html { redirect_to @wg_works }
-      f.json { render :json => @wg_works }
-    end
+    # respond_to do |f|
+    #   f.html { redirect_to @wg_works }
+    #   f.json { render :json => @wg_works }
+    # end
     # render :json => @wg_works
   end
 
@@ -16,12 +16,13 @@ class WgWorksController < ApplicationController
 
   def create
     @wg_work = WgWork.create(wg_work_params)
-    if @wg_work.valid?
+    # if @wg_work
       @wg_work.save
-      redirect_to @wg_work
-    else
-      render :new
-    end
+      redirect_to @wg_work 
+      # wg_work_path(@wg_work)
+    # else
+    #   render :new
+    # end
   end
 
   def show
--- app/serializers/wg_work_serializer.rb
+++
@@ -1,4 +0,0 @@
-class WgWorkSerializer < ActiveModel::Serializer
-  attributes :id, :author, :bio, :title, :version, :content
-  # belongs_to :writer_group
-end
