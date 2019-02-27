a541bdae301b4654891d46ecd1c176c343d349ba
stazman <cadistasio@gmail.com>
2019-02-27 14:21:03 -0600

    creates form for wg_comments/renders wg_comments successfully on wg_work show view; seeds wg_comments; clarifies JS/Rails Project tasks

--- app/controllers/wg_comments_controller.rb
+++ app/controllers/wg_comments_controller.rb
@@ -1,24 +1,23 @@
 class WgCommentsController < ApplicationController
-  def index
-    @wg_comments = WgComment.all
-  end
+  # def index
+  #   @list = List.new
+  #   @wg_comments = WgComment.all
+  # end
 
-  def new
-    @wg_comment = WgComment.new
-  end
+  # def new
+  #   @wg_comment = WgComment.new
+  # end
 
   def create
-    @wg_comment = WgComment.create(wg_comment_params)
-    if @wg_comment.save
-      redirect_to @wg_comment
-    else
-      render :new
-    end
+    @wg_work = WgWork.find(params[:wg_work_id])
+    @wg_comment = @wg_work.wg_comments.build(wg_comment_params)
+    @wg_comment.save
+    redirect_to wg_work_path(@wg_work)
   end
 
-  def show
-    @wg_comment = WgComment.find(params[:id])   
-  end
+  # def show
+  #   @wg_comment = WgComment.find(params[:id])   
+  # end
 
   def edit
   end
