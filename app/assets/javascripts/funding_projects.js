e706fa2516daa43ad5d3c4c3284c7504de2dc04b
stazman <cadistasio@gmail.com>
2019-03-23 23:55:07 -0500

    adds class constructor for funding_project/pledges

--- app/assets/javascripts/funding_projects.js
+++ app/assets/javascripts/funding_projects.js
@@ -8,23 +8,69 @@ $(function () {
   });
 });
 
-$(function () {
-  $("#all_pledges").on('click', function(e) {
+// $(function () {
+//   $("#all_pledges").on('click', function(e) {
     
-    let $ul = $("div.fpPledges ul");
+//     let $ul = $("div.fpPledges ul");
 
-    $ul.html = "";
+//     $ul.html = "";
 
-    $.ajax({
-      method: "GET",
-      url: "/funding_projects/1/pledges",
-      //is not dynamic; only renders the exact url for first funding_project
-      dataType: "json",
-      }).done(function(json){
-        json.forEach(function(pledge){
-        $ul.append("<li>" + pledge.amount + "</li>");
-      });
-    });
-    e.preventDefault();
+//     let id = $(this).data("id");
+
+
+//     $.get("/funding_projects/" + id + "/pledges", function(){
+//         json.forEach(function(pledge){
+//         $ul.append("<li>" + pledge.amount + "</li>");
+//       });
+//     });
+//     e.preventDefault();
+//   });
+// });
+
+function getFundingProjects(){
+  $.ajax({
+      url: "http://localhost:8080/funding_projects",
+      method: 'get',
+      dataType: 'json'
+  }).done(function(data){
+
+      let fpData = new WgWork(data);
+     
+      let fpDataHTML = fpData.wgWorkCommentsHTML();
+
+      document.getElementById("show-wg-work-comments").innerHTML += fpDataHTML;
   });
-});
+}
+
+class FundingProject {
+  constructor(obj){
+      this.id = obj.id
+      this.title = obj.title
+      this.description = obj.description
+      this.funding_goal = obj.funding_goal
+      this.pledges = obj.pledges
+  }
+}
+
+//Calls the wgWorkCommentsHTML method on a prototype of the WgWork class to dynmically render wg_comment data
+FundingProject.prototype.fpPledgesHTML = function (){
+
+  let fpPledges = this.pledges.map(pledge => {
+
+      return (`
+          <p>${pledge.amount}</p>
+          <br>
+      `)
+  });
+
+
+  return (`
+      <div>
+          <h5>Title: ${this.title}</h5>
+          <p>Funding Goal: ${this.funding_goal}</p>
+          <p>${this.description}</p>
+          <br>
+          <h5>Current pledges</h5>
+          <p>${fpPledges}</p>
+      </div>`)
+};
