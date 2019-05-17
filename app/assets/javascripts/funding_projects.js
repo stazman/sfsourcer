b794e0c2397ac977e5721a634a0bcfc1d7f33d32
Christopher DiStasio <cadistasio@gmail.com>
2019-05-17 01:13:04 -0500

    removes more button on funding_projects index view when clicked

--- app/assets/javascripts/funding_projects.js
+++ app/assets/javascripts/funding_projects.js
@@ -1,9 +1,9 @@
 $(function () {
   $(".js-more").on('click', function() {
-    // get the id from the data attribute
     let id = $(this).data("id");
     $.get("/funding_projects/" + id + "/body", function(data) {
       $("#body-" + id).text(data);
+      $(".js-more").remove();
     });
   });
 });
