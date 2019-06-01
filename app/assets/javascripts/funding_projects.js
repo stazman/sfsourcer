a2337243d9d81876ae1d3b9d0848f58801f84080
Christopher DiStasio <cadistasio@gmail.com>
2019-05-31 23:01:29 -0500

    finishes changes re previous commit

--- app/assets/javascripts/funding_projects.js
+++ app/assets/javascripts/funding_projects.js
@@ -10,68 +10,78 @@ $(function () {
 
 $(function () {
   $("#largest_pledge_btn").on('click', function(e) {
-      e.preventDefault();
+    e.preventDefault();
 
-      let fpId = parseInt($("#largest_pledge_btn").attr("data-id"));
+    let fpId = parseInt($("#largest_pledge_btn").attr("data-id"));
 
-      $.get("/funding_projects/" + fpId + "/pledges" + ".json", function(data) {
-        
-        const dataArray = data
+    $.get("/funding_projects/" + fpId + "/pledges" + ".json", function(data) {
+      
+      const dataArray = data
 
-        dataArray.map(pledge => pledge.amount)
+      dataArray.map(pledge => pledge.amount)
 
-        let amountsArr = dataArray.map(pledge => pledge.amount)
-            
-        let largestPledge = Math.max(...amountsArr)
+      let amountsArr = dataArray.map(pledge => pledge.amount)
+          
+      let largestPledge = Math.max(...amountsArr)
 
-        $("#largest_pledge").append(largestPledge)
-      })
-  })
+      $("#largest_pledge").append(largestPledge)
+    });
+  });
 })
 
-$(function () {
-  let pledgeId = parseInt($("#sourcer_credit").attr("data-id"));
-  $.get("/pledges/" + pledgeId + ".json", function(data) {
+//this function kept here for sourcer credit feature to be completed
 
-    let $getCredit = $("#sourcer_credit")
+// $(function () {
 
-    let pledge = data;
+//   let pledgeId = parseInt($("#sourcer_credit").attr("data-id"));
 
-    let pledgeAmount = pledge["amount"]
+//   $.get("/pledges/" + pledgeId + ".json", function(data) {
 
-    function creditMaker (multiplier){
-      return function creditMultiplier (pledgeAmount){
-          multiplier * pledgeAmount
-      }
-    }
+//     let $getCredit = $("#sourcer_credit")
 
-    const multiplyByOnePercent = creditMaker(.01);
+//     let pledge = data;
 
-    multiplyByOnePercent(pledgeAmount);
+//     let pledgeAmount = pledge["amount"]
 
-    if (pledgeAmount <= 50 ){
-    $getCredit.append(multiplyByOnePercent);
-    } else if (pledgeAmount > 50 ){
-      const multiplyByTwoPercent = creditMaker(.02);
-      multiplyByTwoPercent(pledgeAmount);
-      $getCredit.append(multiplyByTwpPercent);
-    }
-  });
-});
+//     function creditMaker (multiplier){
+//       return function creditMultiplier (pledgeAmount){
+//           multiplier * pledgeAmount
+//       };
+//     };
+
+//     const multiplyByOnePercent = creditMaker(.01);
+
+//     multiplyByOnePercent(pledgeAmount);
+
+//     if (pledgeAmount <= 50 ){
+
+//     $getCredit.append(multiplyByOnePercent);
+
+//     } else if (pledgeAmount > 50 ){
+      
+//       const multiplyByTwoPercent = creditMaker(.02);
+      
+//       multiplyByTwoPercent(pledgeAmount);
+      
+//       $getCredit.append(multiplyByTwpPercent);
+//     };
+//   });
+// });
 
 $(function(){
-  $("a.load_all_fps").on("click",function(e){
-
-    $.get(this.href).success(function(json){
+  $("a.load_all_fps").on("click", function(e){
+    $.ajax({
+      url: "http://localhost:8080/funding_projects",
+      method: 'get',
+      dataType: 'json'
+    }).done(function(json){
 
       let $ul = $("div.all_fps ul");
 
-      $ul.html("");
-
       json.forEach(function(funding_project){
-        $ul.append("<li>" + "<a " + "href='" + "/funding_projects/" + funding_project.id + "' >" + funding_project.title + "</li>");
+        $ul.append("<li>" + "<a " + "href='" + "/funding_projects/" + funding_project.id + "' >" + funding_project.title + "</a>" + "</li>");
       });
     });  
     e.preventDefault();
-  })
-})
+  });
+});
