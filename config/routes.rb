c08dfa9c364a360a3c706fd0b004e5d4017aec8e
Christopher DiStasio <cadistasio@gmail.com>
2019-07-03 17:43:39 -0500

    resolves incorrect http request problem by restoring previous organization of routes; removes unneeded files, unused fp_creator fields

--- config/routes.rb
+++ config/routes.rb
@@ -2,50 +2,49 @@ Rails.application.routes.draw do
   
   root 'static#home'
   
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get 'auth/facebook/callback' => 'sessions#create'
-  get 'auth/failure' => 'static#home'
-  get '/logout' => 'sessions#destroy'
+  get '/about', to: 'static#about'
+  get '/contact', to: 'static#contact'
+  get '/terms_and_conditions' => 'static#terms_and_conditions'
+
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get 'auth/facebook/callback', to: 'sessions#create'
+  get 'auth/failure', to: 'static#home'
+  get '/logout', to: 'sessions#destroy'
  
-  get '/funding_projects/:id/description' => 'funding_projects#description'
-  get '/funding_projects/:id/body' => 'funding_projects#body'
-  get '/funding_projects/all_funding_projects' => 'funding_projects#all_funding_projects'
-
-  get '/writer_groups_home' => 'static#writer_groups_home'
+  get '/all_sf_fanvorites', to: 'static#all_sf_fanvorites'
+  get '/sf_lit_fanvorites', to: 'static#sf_lit_fanvorites'
+  get '/lit_fan_authors/alphabetized', to: 'lit_fan_authors#alphabetized'
+  get '/writer_groups_home', to: 'static#writer_groups_home'
   
-  get '/all_sf_fanvorites' => 'static#all_sf_fanvorites'
-  get '/sf_lit_fanvorites' => 'static#sf_lit_fanvorites'
-  get '/lit_fan_authors/alphabetized' => 'lit_fan_authors#alphabetized'
+  get '/funding_projects/:id/description', to: 'funding_projects#description'
 
-  get "/l_stories/all_living_stories" => "l_stories#all_living_stories"
+  get '/funding_projects/:id/body', to: 'funding_projects#body'
+
   patch "l_stories/:id/edit" => "l_stories#update"
 
-  get '/about' => 'static#about'
-  get '/contact' => 'static#contact'
-  get '/terms_and_conditions' => 'static#terms_and_conditions'
+  get "/l_stories/all_living_stories" => "l_stories#all_living_stories"
 
   resources :users
   resources :sessions
-
-  resources :funding_projects
-  resources :pledges
- 
   resources :lit_fan_genres
   resources :lit_fan_works
   resources :lit_fan_authors
-
   resources :l_stories
   resources :ls_comments
   resources :ls_users
-
+  resources :funding_projects
+  resources :pledges
   resources :writer_groups
   resources :wg_works
   resources :wg_comments
-
   resources :events
   
-  
+  resources :lit_fan_authors do
+    resources :lit_fan_works, only: [:index, :new, :show, :edit]
+  end
+
   resources :funding_projects do
     resources :pledges
   end
@@ -57,9 +56,4 @@ Rails.application.routes.draw do
   resources :wg_works do
     resources :wg_comments
   end
-
-  resources :lit_fan_authors do
-    resources :lit_fan_works, only: [:index, :new, :show, :edit]
-  end
-
 end
