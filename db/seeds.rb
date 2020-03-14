d54b145d86daef187e48c80e0cdc5f4e74b48778
Christopher DiStasio <cadistasio@gmail.com>
2020-03-14 17:06:19 -0500

    Edits seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,15 @@
 
 # author_1 = LitFanAuthor.create!(name: "Frank Herbert")
+# work1 = LitFanWork.create!(title: "Dune")
+# genre1 = LitFanGenre.create!(name: "Science Fiction")
 
 ls_user = LsUser.create!(username: "Funky Fred")
 ls_1 = LStory.create!(title: "From the Basement", content: "I heard a noise from the basement")
 ls_2 = LStory.create!(title: "You, Robot", content: "Why is there a robot eating my foot, I wondered")
 lsc_1 = LsComment.create!(content: "ghost!", ls_user_id: 1, l_story_id: 1)
 wg_1 = WriterGroup.create!(wg_creator: "Christopher DiStasio", wg_name: "Science-Fantasy", wg_description: "We look forward to helping each other improve our writing in a friendly, productive environment. The work of writers in this section tend to have elements of both hard science fiction and fantasy.")
-# b_fp = FundingProject.create!(title: "Test Fp", description: "test", funding_goal: "200")
-# a_fpcreator = FpCreator.create!(creator_name: "ac", location: "tampa")
-# # a_pledge = Pledge.create(amount: 20)
-# a_backer = FpBacker.create!(name: "a", backer_email: "a@a.com")
-b_pledge = Pledge.create!(amount: 10, funding_project_id: 1, fp_backer_id: 1)
 
-# work1 = LitFanWork.create!(title: "Dune")
-# genre1 = LitFanGenre.create!(name: "Science Fiction")
+a_pledge = Pledge.create!(amount: 10, funding_project_id: 1, fp_backer_id: 1)
 
 wg_work1 = WgWork.create!(author: "test", bio: "test", title: "test", version: "1", content: "a", writer_group_id: 1)
 
